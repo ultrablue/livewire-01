3a23fff485abff42d2f604d4bc8c63486085afde
Greg Gómez <gogogo@unm.edu>
2021-08-27 03:54:01 +0000

    I might have messed up the repo. :(

--- resources/views/components-tests/components-02.blade.php
+++ resources/views/components-tests/components-02.blade.php
@@ -13,8 +13,6 @@
     </div>
 
 
-    {{-- {{ dump($states) }} --}}
-
     <div class="m-10">
         <select class="block appearance-none w-auto bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             id="grid-state">
