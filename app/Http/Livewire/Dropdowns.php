af458d4f7049860b0373537ed7728e2bfabb08cb
Greg Gomez <gogogo@unm.edu>
2021-09-08 02:45:03 +0000

    Latest changes.

--- app/Http/Livewire/Dropdowns.php
+++ app/Http/Livewire/Dropdowns.php
@@ -12,22 +12,15 @@ class Dropdowns extends Component
     public $books = [];
     public $book;
 
+    public function mount()
+    {
+    }
+
     public function render()
     {
         if (!empty($this->author)) {
             $this->books = Book::where('author_id', $this->author)->get();
         }
-
-        // We do get some books. Uncomment after the inital load, but before making an Author selection. It's just getting bound to the variable.
-        // if (count($this->books)) {
-        //     dd($this->books);
-        // }
-
-        // Can we get it to send anything??? Nope. This only works once.
-        // $this->books = Book::where('author_id', rand(1, 3))->get();
-
-        // This doesn't work:
-        // return view('livewire.dropdowns', ['authors' => Author::orderBy('last_name')->get(), 'books' => $this->books]);
 
         return view('livewire.dropdowns')->withAuthors(Author::orderBy('last_name')->get());
     }
--- resources/views/livewire/dropdowns.blade.php
+++ resources/views/livewire/dropdowns.blade.php
@@ -1,19 +1,25 @@
 <div> {{-- All Livewire components must have one, and only one, root element! --}}
 
     <div class="border-t-2 border-solid border-black m-4 p-4">
-        <select wire:model="author" name="author">
-            <option value="">Choose an author</option>
+        <select wire:model="author" name="author" class="block appearance-none w-auto bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+            <option value="" >Choose an author</option>
             @foreach ($authors as $author)
-                <option value="{{ $author->id }}">{{ $author->first_name }} {{ $author->last_name }}</option>
+                <option value="{{ $author->id }}" class="bg-blue-200">{{ $author->first_name }} {{ $author->last_name }}</option>
             @endforeach
         </select>
     </div>
 
 
     @if (count($books) > 0)
+
+<div class="m-10 border-2">
+Author: {{ $author }}
+</div>
+
+
         <div class="m-4 p-4">
+            <select name="book" wire:model="book" class="block appearance-none w-auto bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
             <option value="">Choose a book</option>
-            <select name="book" wire:model="book">
                 @foreach ($books as $book)
                     <option value={{ $book->id }}>{{ $book->title }}</option>
                 @endforeach
