07e4f33f341ca8f6f1a50da3846bfaa396fe6295
Greg Gomez <gogogo@unm.edu>
2021-09-12 15:46:14 +0000

    🗃 Adds generated migrations files.

---
+++ tests/database/migrations/2021_09_12_154434_create_states_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('states', function (Blueprint $table) {
+            $table->string('state', 22);
+            $table->char('state_code', 2)->primary();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('states');
+    }
+}
