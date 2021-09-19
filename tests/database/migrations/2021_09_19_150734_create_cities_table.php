611435a1d7b5d7dad8a12739f8352804b45777e3
Greg Gomez <gogogo@unm.edu>
2021-09-19 15:09:23 +0000

    Oops I thought I hadn't done this for all the tables. Turned out I have.

---
+++ tests/database/migrations/2021_09_19_150734_create_cities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->integer('id')->primary();
+            $table->string('city', 50);
+            $table->char('state_code', 2);
+            $table->unsignedInteger('zip');
+            $table->double('latitude');
+            $table->double('longitude');
+            $table->string('county', 50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}
